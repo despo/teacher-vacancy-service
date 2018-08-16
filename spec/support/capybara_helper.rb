dbdd1d38b4ccb79375c5b1968d54bc0d2a43ee5a
Gaz Aston <gaz@dxw.com>
2018-08-16 14:11:19 +0100

    (fix) cabybara helper xpath traversal

--- spec/support/capybara_helper.rb
+++ spec/support/capybara_helper.rb
@@ -1,10 +1,10 @@
 module CapybaraHelper
   def click_link_in_container_with_text(text)
-    find(:xpath, "//tr[td[contains(text(), '#{text}')]]").find('td.cya-change a').click
+    find(:xpath, "//div[dt[contains(text(), '#{text}')]]").find('.app-check-your-answers__change a').click
   end
 
   def within_row_for(element: 'label', text:, &block)
-    element = page.find(element, text: text).find(:xpath, '..')
+    element = page.find(element, text: text).find(:xpath, '../..')
     within(element, &block)
   end
 end
